fb5d8996b8bcf632f77555a4d891da1190f28d08
Vampika <vampika.vamp@gmail.com>
2025-01-29 12:03:44 +0300

    update pathes

--- assetsMap.js
+++ assetsMap.js
@@ -2,43 +2,43 @@ export const assetsMap = {
     sprites: [
         {
             alias: "GunConnectorD",
-            src: "../test-project/assets/parts/gun_connectors/GunConnectorD.png",
+            src: "../assets/parts/gun_connectors/GunConnectorD.png",
         },
         {
             alias: "GunConnectorA",
-            src: "../test-project/assets/parts/gun_connectors/GunConnectorA.png",
+            src: "../assets/parts/gun_connectors/GunConnectorA.png",
         },
         {
             alias: "GunConnectorB",
-            src: "../test-project/assets/parts/gun_connectors/GunConnectorB.png",
+            src: "../assets/parts/gun_connectors/GunConnectorB.png",
         },
         {
             alias: "HeavyGunB",
-            src: "../test-project/assets/parts/guns/HeavyGunB.png",
+            src: "../assets/parts/guns/HeavyGunB.png",
         },
         {
             alias: "MediumHullA",
-            src: "../test-project/assets/parts/hulls/MediumHullA.png",
+            src: "../assets/parts/hulls/MediumHullA.png",
         },
         {
             alias: "HeavyTowerB",
-            src: "../test-project/assets/parts/towers/HeavyTowerB.png",
+            src: "../assets/parts/towers/HeavyTowerB.png",
         },
         {
             alias: "TrackAFrame1",
-            src: "../test-project/assets/parts/tracks/TrackAFrame1.png",
+            src: "../assets/parts/tracks/TrackAFrame1.png",
         },
         {
             alias: "TrackAFrame2",
-            src: "../test-project/assets/parts/tracks/TrackAFrame2.png",
+            src: "../assets/parts/tracks/TrackAFrame2.png",
         },
         {
             alias: "MediumShell",
-            src: "../test-project/assets/parts/bullets/MediumShell.png",
+            src: "../assets/parts/bullets/MediumShell.png",
         },
         {
             alias: "Background",
-            src: "../test-project/assets/parts/background/Background.png",
+            src: "../assets/parts/background/Background.png",
         },
     ],
 };
